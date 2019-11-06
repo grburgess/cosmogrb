078377ee2c85bd14ac0b5fa63cf7baac2a654f11
grburgess <jmichaelburgess@gmail.com>
2019-11-06 13:29:55 +0100

    finished tte file

--- cosmogrb/lightcurve/tte.py
+++ cosmogrb/lightcurve/tte.py
@@ -62,7 +62,9 @@ class EBOUNDS(FITSExtension):
         ("DETNAM", None, "Individual detector name"),
     )
 
-    def __init__(self, det_name ,tstart, tstop, trigger_time, ra, dec, channel, emin, emax):
+    def __init__(
+        self, det_name, tstart, tstop, trigger_time, ra, dec, channel, emin, emax
+    ):
 
         data_list = [("CHANNEL", channel), ("E_MIN", emin), ("E_MAX", emax)]
 
@@ -121,11 +123,11 @@ class EVENTS(FITSExtension):
         ("DETNAM", None, "Individual detector name"),
     )
 
-    def __init__(self, tstart, tstop, trigger_time, ra, dec, pha, time):
+    def __init__(self, det_name, tstart, tstop, trigger_time, ra, dec, pha, time):
 
         data_list = [("PHA", pha), ("TIME", time)]
 
-        super(EBOUNDS, self).__init__(tuple(data_list), self._HEADER_KEYWORDS)
+        super(EVENTS, self).__init__(tuple(data_list), self._HEADER_KEYWORDS)
 
         self.hdu.header.set("RA_OBJ", ra)
         self.hdu.header.set("DEC_OBJ", dec)
@@ -135,7 +137,91 @@ class EVENTS(FITSExtension):
         self.hdu.header.set("TZERO1", trigger_time)
         self.hdu.header.set("PRMT_BEG", trigger_time)
         self.hdu.header.set("FIFO_END", tstop)
+        self.hdu.header.set("DETNAM", _det_translate[det_name])
 
 
 class GTI(FITSExtension):
-    pass
+
+    _HEADER_KEYWORDS = (
+        ("EXTNAME", "GTI", "Extension name"),
+        ("CONTENT", "OGIP PHA data", "File content"),
+        ("HDUCLASS", "OGIP    ", "format conforms to OGIP standard"),
+        ("HDUVERS", "1.1.0   ", "Version of format (OGIP memo CAL/GEN/92-002a)"),
+        (
+            "HDUDOC",
+            "OGIP memos CAL/GEN/92-002 & 92-002a",
+            "Documents describing the forma",
+        ),
+        ("HDUVERS1", "1.0.0   ", "Obsolete - included for backwards compatibility"),
+        ("HDUVERS2", "1.1.0   ", "Obsolete - included for backwards compatibility"),
+        ("HDUCLAS1", "GTI", "Extension constains events  "),
+        ("FILTER", "", "Filter used"),
+        ("TELESCOP", "GLAST", "Name of mission/satelite"),
+        ("INSTRUME", "GBM", "Specific instrument used for observation"),
+        ("DATE", "2009-05-19T18:49:32", "file creation date"),
+        ("DATE-OBS", "2009-05-19T18:49:32", "Date of start of observation"),
+        ("DATE-END", "2009-05-19T18:49:32", "Date of end of observation"),
+        ("OBSERVER", "God", "The creator of the Universe"),
+        ("OBJECT", "GRB080916200", "Burst name in standard format"),
+        ("ORIGIN", "MPE", "Name of organization creating the file"),
+        ("TIMESYS", "TT", "Time system used in keywords"),
+        ("TIMEUNIT", "s", "Time since MJDREF, used in TSTART and TSTOP"),
+        ("MJDREFI", 51910, "MJD of GLAST reference epoch"),
+        ("MJDREFI", 7.428703703703703e-4, "MJD of GLAST reference epoch"),
+        ("RADECSYS", "FK5     ", "Stellar reference frame"),
+        ("EQUINOX", 2000.0, "Equinox of RA and Dec"),
+        ("RA_OBJ", None, "Calculated RA of burst"),
+        ("DEC_OBJ", None, "Calculated Dec of burst"),
+        ("ERR_RAD", 3.000, "Calculated Location of Error Radius"),
+        ("EXTVER", 1, ""),
+        ("CH2E_VER", "SPLINE 2.0", ""),
+        ("GAIN_COR", 1.0, ""),
+        ("TSTART", None, "[GLAST MET] Observation start time"),
+        ("TSTOP", None, "[GLAST MET] Observation stop time"),
+        ("TRIGTIME", None, "Trigger time realtive to MJDREF, double precision"),
+        ("TZERO1", None, "Time offset"),
+        ("DETNAM", None, "Individual detector name"),
+    )
+
+    def __init__(self, det_name, tstart, tstop, trigger_time, ra, dec):
+
+        data_list = [("START", np.array([tstart])), ("STOP", np.array([tstop]))]
+
+        super(GTI, self).__init__(tuple(data_list), self._HEADER_KEYWORDS)
+
+        self.hdu.header.set("RA_OBJ", ra)
+        self.hdu.header.set("DEC_OBJ", dec)
+        self.hdu.header.set("TSTART", tstart)
+        self.hdu.header.set("TSTOP", tstop)
+        self.hdu.header.set("TRIGTIME", trigger_time)
+        self.hdu.header.set("TZERO1", trigger_time)
+        self.hdu.header.set("DETNAM", _det_translate[det_name])
+
+
+class TTEFile(FITSFile):
+    def __init__(
+        self,
+        det_name,
+        tstart,
+        tstop,
+        trigger_time,
+        ra,
+        dec,
+        channel,
+        emin,
+        emax,
+        pha,
+        time,
+    ):
+
+        ebounds_extension = EBOUNDS(
+            det_name, tstart, tstop, trigger_time, ra, dec, channel, emin, emax
+        )
+        events_extension = EVENTS(
+            det_name, tstart, tstop, trigger_time, ra, dec, channel, pha, time
+        )
+        gti_extension = GTI(det_name, tstart, tstop, trigger_time, ra, dec)
+
+        super(TTEFile, self).__init__(
+            fits_extensions=[ebounds_extension, events_extension, gti_extension]
+        )

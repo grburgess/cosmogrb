ffd8638a2dde56aea53792cd859c447c1375705d
grburgess <jmichaelburgess@gmail.com>
2020-02-25 08:50:50 +0100

    remove jit and speed up test

--- cosmogrb/test/test_basic.py
+++ cosmogrb/test/test_basic.py
@@ -5,10 +5,10 @@ def test_basic_gbm():
 
     grb = GBMGRB(ra=312., dec =-62. ,
              z=1.,
-             peak_flux=5E-7, alpha= -.66, ep=500., 
+             peak_flux=5E-9, alpha= -.66, ep=500., 
              tau=2.,
-             trise=1.,
-             duration=20.,
+             trise=.1,
+             duration=1.,
              T0=0.1,
              
              verbose=True)

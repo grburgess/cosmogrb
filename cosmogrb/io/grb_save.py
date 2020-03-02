c10de7396cf48420ea1a1c4a460b27a9ce156690
grburgess <jmichaelburgess@gmail.com>
2020-03-02 19:21:06 +0100

    adding grb save object

---
+++ cosmogrb/io/grb_save.py
@@ -0,0 +1,110 @@
+import h5py
+from cosmogrb.lightcurve.lightcurve import LightCurveStorage
+from cosmogrb.response import Response
+
+
+class GRBSave(object):
+    def __init__(self, grb_name, lightcurves, responses):
+
+        assert isinstance(
+            lightcurves, dict
+        ), "lightcurves must be a dict containing LightCurveStorage objects"
+
+        assert isinstance(
+            responses, dict
+        ), "responses must be a dict containing Response objects"
+
+        for k, v in lightcurves.items():
+
+            assert isinstance(v, LightCurveStorage), print(
+                f"{k} is not of type LightCurveStorage"
+            )
+
+        for k, v in responses.items():
+
+            assert isinstance(v, Response), print(f"{k} is not of type Response")
+
+        for key1, key2 in zip(responses.keys(), lightcurves.keys()):
+
+            assert (
+                key1 == key2
+            ), f"{key1} is not {key2} so there is a mismatch between the responses and lightcurves"
+
+        # ok, now lets build the objects
+
+        self._internal_storage = dict()
+
+        for key in lightcurves.keys():
+
+            self._internal_storage[key] = dict(
+                lightcurve=lightcurves[key], response=responses[key]
+            )
+
+        self._name = grb_name
+
+        
+
+        
+    @property
+    def name(self):
+        return self._name
+
+    @classmethod
+    def from_file(cls, file_name):
+
+        lightcurves = dict()
+        responses = dict()
+
+        with h5py.File(file_name, "r") as f:
+
+            grb_name = f.attrs["grb_name"]
+
+            det_group = f["detectors"]
+
+            for lc_name in det_group.keys():
+
+                lc_group = det_group[lc_name]
+
+                channels = lc_group["channels"]
+
+                pha = lc_group["total_signal"]["pha"][()]
+                times = lc_group["total_signal"]["times"][()]
+
+                pha_source = lc_group["source_signal"]["pha"][()]
+                times_source = lc_group["source_signal"]["times"][()]
+
+                pha_background = lc_group["background_signal"]["pha"][()]
+                times_background = lc_group["background_signal"]["times"][()]
+
+                # now get the response info
+
+                rsp_group = lc_group["response"]
+
+                matrix = rsp_group["matrix"][()]
+                energy_edges = rsp_group["energy_edges"][()]
+                channel_edges = rsp_group["channel_edges"][()]
+                geometric_area = rsp_group.attrs["geometric_area"]
+
+                rsp = Response(
+                    matrix=matrix,
+                    geometric_area=geometric_area,
+                    energy_edges=energy_edges,
+                    channel_edges=channel_edges,
+                )
+
+                responses[lc_name] = rsp
+
+                lc_container = LightCurveStorage(
+                    pha=pha,
+                    times=times,
+                    times_source=times_source,
+                    pha_source=pha_source,
+                    times_background=times_background,
+                    pha_background=pha_background,
+                    channels=rsp.channels,
+                    ebounds=rsp.channel_edges,
+                )
+
+                lightcurves[lc_name] = lc_container
+
+        return cls(grb_name=grb_name, lightcurves=lightcurves, responses=responses)

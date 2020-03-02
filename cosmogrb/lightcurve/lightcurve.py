e5eab7f25be9593bafbec1370d025ca74e2e57e3
grburgess <jmichaelburgess@gmail.com>
2020-03-02 11:53:40 +0100

    added lightcurve container class

--- cosmogrb/lightcurve/lightcurve.py
+++ cosmogrb/lightcurve/lightcurve.py
@@ -130,6 +130,19 @@ class LightCurve(object):
 
         logger.debug(f"{self._grb_name} {self._name}: now has {sum(self._pha)} counts")
 
+        # now create a lightcurve storage
+
+        self._lc_storage = LightCurveStorage(
+            pha=self._pha,
+            times=self._times,
+            pha_source=self._pha_source,
+            times_source=self._times_source,
+            pha_background=self._pha_background,
+            times_background=self._times_background,
+            channels=self._channels,
+            ebounds=self._ebounds,
+        )
+
     def display_energy_dependent_light_curve(
         self, time, energy, ax=None, cmap="viridis", **kwargs
     ):
@@ -191,3 +204,84 @@ class LightCurve(object):
     @property
     def name(self):
         return self._name
+
+
+class LightCurveStorage(object):
+    def __init__(
+        self,
+        pha,
+        times,
+        pha_source,
+        times_source,
+        pha_background,
+        times_background,
+        channels,
+        ebounds,
+        T0=0,
+    ):
+        """
+        Container class for light curve objects
+
+        :param pha: 
+        :param times: 
+        :param pha_source: 
+        :param times_source: 
+        :param pha_background: 
+        :param times_background: 
+        :param channels: 
+        :param ebounds: 
+        :param T0: 
+        :returns: 
+        :rtype: 
+
+        """
+
+        self._pha = pha
+        self._times = times + T0
+
+        self._pha_source = pha_source
+        self._times_source = times_source + T0
+
+        self._pha_background = pha_background
+        self._times_background = times_background + T0
+
+        self._channels = channels
+        self._ebounds = ebounds
+
+        self._T0 = T0
+
+    @property
+    def T0(self):
+        return self._T0
+
+    @property
+    def channels(self):
+        return self._channels
+
+    @property
+    def ebounds(self):
+        return self._ebounds
+
+    @property
+    def times(self):
+        return self._times
+
+    @property
+    def pha(self):
+        return self._pha
+
+    @property
+    def pha_source(self):
+        return self._pha_source
+
+    @property
+    def times_source(self):
+        return self._times_source
+
+    @property
+    def pha_background(self):
+        return self._pha_background
+
+    @property
+    def times_background(self):
+        return self._times_background

71a7b03d3b8716db5a4ef8441424f6b29ae2dd39
grburgess <jmichaelburgess@gmail.com>
2020-03-04 14:14:44 +0100

    break up tests
    Modified-by: grburgess <jmichaelburgess@gmail.com>

--- cosmogrb/test/test_basic.py
+++ cosmogrb/test/test_basic.py
@@ -4,7 +4,7 @@ from glob import glob
 from cosmogrb.grb import GBMGRB, GBMGRB_CPL
 
 
-def test_basic_gbm():
+def test_gbm_constructor_and_plotting():
 
     grb = GBMGRB_CPL(
         ra=312.0,
@@ -25,11 +25,49 @@ def test_basic_gbm():
     grb.display_energy_dependent_light_curve(time, energy)
     grb.display_energy_integrated_light_curve(time)
 
+
+def test_gbm_process():
+
+    grb = GBMGRB_CPL(
+        ra=312.0,
+        dec=-62.0,
+        z=1.0,
+        peak_flux=5e-9,
+        alpha=-0.66,
+        ep=500.0,
+        tau=2.0,
+        trise=0.1,
+        duration=1.0,
+        T0=0.1,
+    )
+
     grb.go(n_cores=1)
 
+    files_to_remove = glob("SynthGRB*.rsp")
 
-    grb.save('test.h5')
-    
+    for f in files_to_remove:
+        os.remove(f)
+
+
+def test_gbm_save():
+
+    grb = GBMGRB_CPL(
+        ra=312.0,
+        dec=-62.0,
+        z=1.0,
+        peak_flux=5e-9,
+        alpha=-0.66,
+        ep=500.0,
+        tau=2.0,
+        trise=0.1,
+        duration=1.0,
+        T0=0.1,
+    )
+
+    grb.go(n_cores=1)
+
+    grb.save("test.h5")
+
     files_to_remove = glob("SynthGRB*.rsp")
 
     for f in files_to_remove:

64dadce58ebe649aebaacb50bc0c3aa869a58ff0
grburgess <jmichaelburgess@gmail.com>
2020-03-04 19:51:00 +0100

    added some lc testing

---
+++ cosmogrb/test/test_lightcurve.py
@@ -0,0 +1,17 @@
+from cosmogrb.utils.package_utils import get_path_of_data_file
+from cosmogrb.io.grb_save import GRBSave
+
+
+
+def test_lightcurve_plotting():
+
+    grb = GRBSave.from_file(get_path_of_data_file('grb.h5'))
+
+    for key in grb.keys:
+
+        lc = grb[key]['lightcurve']
+
+        lc.display_lightcurve()
+        lc.display_background()
+        lc.display_source()
+    

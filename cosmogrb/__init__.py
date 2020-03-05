6576f21d944bf86e0d0ffaa197c12d14cc08a5f1
grburgess <jmichaelburgess@gmail.com>
2020-03-05 08:05:53 +0100

    fix import

--- cosmogrb/__init__.py
+++ cosmogrb/__init__.py
@@ -1,4 +1,4 @@
 from cosmogrb.grb import GRB, GBMGRB, GBMGRB_CPL
-from cosmogrb.io import GRBSave, gbm_fits
+from cosmogrb.io import GRBSave, grbsave_to_gbm_fits
 
-__all__ = ["GRB", "GBMGRB", "GBMGRB_CPL", "GRBSave", "gbm_fits"]
+__all__ = ["GRB", "GBMGRB", "GBMGRB_CPL", "GRBSave", "grbsave_to_gbm_fits"]

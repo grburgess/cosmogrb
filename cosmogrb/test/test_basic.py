c5311952cc37379916d791215017e8e4e7fc4517
grburgess <jmichaelburgess@gmail.com>
2020-03-10 06:51:07 +0100

    updated tests
    Former-commit-id: ed71a2b949fefb77ed4dbdf891907e703677af75

--- cosmogrb/test/test_basic.py
+++ cosmogrb/test/test_basic.py
@@ -6,20 +6,33 @@ from cosmogrb.io.grb_save import GRBSave
 from cosmogrb.io.gbm_fits import grbsave_to_gbm_fits
 
 
-def test_gbm_constructor_and_plotting():
+import pytest
 
-    grb = GBMGRB_CPL(
-        ra=312.0,
-        dec=-62.0,
-        z=1.0,
-        peak_flux=5e-9,
-        alpha=-0.66,
-        ep=500.0,
-        tau=2.0,
-        trise=0.1,
-        duration=1.0,
-        T0=0.1,
+@pytest.fixture(scope='session')
+def grb():
+    grb = GBMGRB_CPL(ra=312.0,
+                     dec=-62.0,
+                     z=1.0,
+                     peak_flux=5e-9,
+                     alpha=-0.66,
+                     ep=500.0,
+                     tau=2.0,
+                     trise=0.1,
+                     duration=1.0,
+                     T0=0.1,
     )
+
+    grb.go(n_cores=8)
+    
+    return grb
+
+
+
+
+
+
+
+def test_gbm_constructor_and_plotting(grb):
 
     time = np.linspace(0, 20, 10)
     energy = np.logspace(1, 2, 10)
@@ -28,43 +41,17 @@ def test_gbm_constructor_and_plotting():
     grb.display_energy_integrated_light_curve(time)
 
 
-def test_gbm_process():
+# def test_gbm_process(grb):
 
-    grb = GBMGRB_CPL(
-        ra=312.0,
-        dec=-62.0,
-        z=1.0,
-        peak_flux=5e-9,
-        alpha=-0.66,
-        ep=500.0,
-        tau=2.0,
-        trise=0.1,
-        duration=1.0,
-        T0=0.1,
-    )
+    
+    
+#     files_to_remove = glob("SynthGRB*.rsp")
 
-    grb.go(n_cores=1)
-
-    files_to_remove = glob("SynthGRB*.rsp")
-
-    for f in files_to_remove:
-        os.remove(f)
+#     for f in files_to_remove:
+#         os.remove(f)
 
 
-def test_gbm_save():
-
-    grb = GBMGRB_CPL(
-        ra=312.0,
-        dec=-62.0,
-        z=1.0,
-        peak_flux=5e-9,
-        alpha=-0.66,
-        ep=500.0,
-        tau=2.0,
-        trise=0.1,
-        duration=1.0,
-        T0=0.1,
-    )
+def test_gbm_save(grb):
 
     grb.go(n_cores=1)
 

9d538fc89dda8b5789f37547555431733a8e9c3a
grburgess <jmichaelburgess@gmail.com>
2020-03-10 08:38:23 +0100

    added channel plot

---
+++ cosmogrb/utils/plotting/channel_plot.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from cosmogrb.utils.plotting import step_plot
+
+
+def channel_plot(ax, chan_min, chan_max, counts, **kwargs):
+    """FIXME! briefly describe function
+
+    :param ax: 
+    :param chan_min: 
+    :param chan_max: 
+    :param counts: 
+    :returns: 
+    :rtype: 
+
+    """
+    
+    chans = np.vstack([chan_min, chan_max]).T
+    width = chan_max - chan_min
+
+    step_plot(chans, counts / width, ax, **kwargs)
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+
+    ax.set_xlabel("Channel Energy")
+
+    ax.set_xlabel("cnts/s/energy")

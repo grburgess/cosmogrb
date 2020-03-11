64d41c9c4599af892705e90c98bc126e2343408f
grburgess <jmichaelburgess@gmail.com>
2020-03-11 17:52:18 +0100

    updated some things

--- cosmogrb/config.py
+++ cosmogrb/config.py
@@ -2,7 +2,7 @@ from cosmogrb.utils.file_utils import (
     file_existing_and_readable,
     if_directory_not_existing_then_make,
 )
-from cosmogrb.utils.package_data import get_path_of_user_dir, get_path_of_data_file
+from cosmogrb.utils.package_utils import get_path_of_user_dir, get_path_of_data_file
 import yaml
 import os
 import shutil

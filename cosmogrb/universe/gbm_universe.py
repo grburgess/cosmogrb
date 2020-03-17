b20e5ef84d4c79dd48bc20e7130b3a8161190030
grburgess <jmichaelburgess@gmail.com>
2020-03-17 14:37:00 +0100

    fix missing argument
    Former-commit-id: 22f3d0a44a284730ef5783cbb2bf58e50651a1f0

--- cosmogrb/universe/gbm_universe.py
+++ cosmogrb/universe/gbm_universe.py
@@ -12,8 +12,8 @@ class GBM_CPL_Universe(Universe):
 
         super(GBM_CPL_Universe, self).__init__(population)
 
-    def _grb_wrapper(self, parameter_server):
-        return GBM_CPL_Wrapper(parameter_server)
+    def _grb_wrapper(self, parameter_server, serial=False):
+        return GBM_CPL_Wrapper(parameter_server, serial=serial)
 
     def _process_populations(self):
 

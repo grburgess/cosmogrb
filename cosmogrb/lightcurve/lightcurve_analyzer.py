ef997654392a63f5a26c8a15338c7fc6e2cd1cf8
grburgess <jmichaelburgess@gmail.com>
2020-03-22 08:05:00 +0100

    semi-working gbm detector

--- cosmogrb/lightcurve/lightcurve_analyzer.py
+++ cosmogrb/lightcurve/lightcurve_analyzer.py
@@ -6,9 +6,12 @@ class LightCurveAnalyzer(object, metaclass=abc.ABCMeta):
 
         assert isinstance(lightcurve, LightCurveStorage)
 
-    
+
+        self._lightcurve = lightcurve
         
         self._is_detected = False
+
+        self._process_dead_time()
 
     @abc.abstractmethod
     def _compute_detection(self):
@@ -24,6 +27,21 @@ class LightCurveAnalyzer(object, metaclass=abc.ABCMeta):
         return self._is_detected
 
     @abc.abstractmethod
-    def dead_time_per_interval(self):
+    def dead_time_of_interval(self, tmax, tmin):
 
         pass
+
+
+    def exposure_of_interval(self, tmin, tmax):
+        """
+        return the exposure of the interval
+
+
+        :param tmin: 
+        :param tmax: 
+        :returns: 
+        :rtype: 
+
+        """
+
+        return (tmax - tmin) - self.dead_time_per_interval(tmin, tmax)

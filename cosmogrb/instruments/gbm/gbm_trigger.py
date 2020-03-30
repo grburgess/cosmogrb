fdc145b5ca0dad713234ef0be8c88da73513a12b
grburgess <jmichaelburgess@gmail.com>
2020-03-31 00:14:49 +0200

    added gbm trigger and test
    Former-commit-id: 050d6a8ecb9d4d7c120e3afc76a37cb74298c79b

---
+++ cosmogrb/instruments/gbm/gbm_trigger.py
@@ -0,0 +1,139 @@
+import numpy as np
+from cosmogrb.instruments.gbm.gbm_lightcurve_analyzer import GBMLightCurveAnalyzer
+from cosmogrb import GRBSave
+import coloredlogs, logging
+import cosmogrb.utils.logging
+
+logger = logging.getLogger("cosmogrb.gbm.trigger")
+
+class GBMTrigger(object):
+    def __init__(self, grb_save, threshold=4.5, simul_trigger_window=0.5):
+
+        self._grb_save = GRBSave.from_file(grb_save)
+
+        self._threshold = threshold
+        self._simul_trigger_window = simul_trigger_window
+
+        self._is_detected = False
+        self._trigger_times = []
+        self._triggered_detectors = []
+
+        
+        # sort the detectors by their distance to the
+        # grb
+        self._setup_order_by_distance()
+
+
+    def _setup_order_by_distance(self):
+
+        # now we will go through the lightcurves
+        # collect
+
+        logger.debug(f"{self._grb_save.name} is having its detectors ordered ")
+        
+        angular_distances = []
+        lc_names = []
+        
+        for name in self._grb_save.keys:
+
+            if name.startswith("n"):
+
+                lc = self._grb_save[name]["lightcurve"]
+                lc_names.append(name)
+
+                angular_distances.append(lc.extra_info["angle"])
+
+        angular_distances = np.array(angular_distances)
+        lc_names = np.array(lc_names)
+
+        # now attach the lc_names sorted by
+        # the detector distance to the GRB
+
+        idx = angular_distances.argsort()
+        self._lc_names = lc_names[idx]
+
+    @property
+    def triggered_detectors(self):
+        return self._triggered_detectors
+
+    @property
+    def triggered_times(self):
+        return self._trigger_times
+
+    @property
+    def is_detected(self):
+        return self._is_detected
+
+    def _check_simultaneous_triggers(self, time):
+
+        # go thru all the other trigger times and see
+        # if this one is within the window of others
+
+        detected = False
+
+        for other_time in self._trigger_times:
+
+            if (time >= (other_time - self._simul_trigger_window)) and (
+                time <= (other_time + self._simul_trigger_window)
+            ):
+                # we found one!
+
+                detected = True
+
+                break
+        # nothing close
+
+        return detected
+
+    def process_triggers(self):
+        """
+        Process the GBM detectors to find
+        two simultaneous triggers
+
+        :returns: 
+        :rtype: 
+
+        """
+
+        n_triggered = 0
+
+        n_tested = 0
+
+ 
+        while (n_tested < len(self._lc_names)) and (not self._is_detected):
+
+            lc = self._grb_save[self._lc_names[n_tested]]["lightcurve"]
+
+            print(n_tested)
+
+            lc_analyzer = GBMLightCurveAnalyzer(
+                lightcurve=lc, threshold=self._threshold
+            )
+
+            if lc_analyzer.is_detected:
+
+                # we saw something!
+                # add the name and the time
+
+                
+
+                if n_triggered != 0:
+
+                    # check the other trigger times to
+                    # see if they are close to this one
+
+                    if self._check_simultaneous_triggers(lc_analyzer.detection_time):
+
+                        # ok, we found at least two triggers nearly the same time
+                        self._is_detected = True
+                         
+
+                    
+
+                self._triggered_detectors.append(self._lc_names[n_tested])
+                self._trigger_times.append(lc_analyzer.detection_time)
+                
+
+                n_triggered += 1
+
+            n_tested += 1

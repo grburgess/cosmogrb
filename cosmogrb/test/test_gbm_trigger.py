1654d6971ae976d5b1909b981a61d0a5919c069c
grburgess <jmichaelburgess@gmail.com>
2020-03-31 00:14:49 +0200

    added gbm trigger and test

---
+++ cosmogrb/test/test_gbm_trigger.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+def test_gbm_trigger_constructor(gbm_trigger):
+
+    assert not gbm_trigger.is_detected
+    assert len(gbm_trigger.triggered_detectors) == 0
+    assert len(gbm_trigger.triggered_times) == 0
+    assert len(gbm_trigger._lc_names) == 12
+
+    proper_order = ['n1', 'n0', 'n2', 'n5', 'n9', 'n3', 'n6', 'na', 'n7', 'n4', 'nb', 'n8']
+
+    for x,y in zip(gbm_trigger._lc_names, proper_order):
+        assert x == y
+
+def test_gbm_trigger_process(gbm_trigger):
+
+    gbm_trigger.process_triggers()
+
+    assert gbm_trigger.is_detected
+    assert len(gbm_trigger.triggered_times) == 2
+    assert len(gbm_trigger.triggered_detectors) == 2
+    assert gbm_trigger.triggered_detectors[0] == 'n1'
+    assert gbm_trigger.triggered_detectors[1] == 'n0'
+    

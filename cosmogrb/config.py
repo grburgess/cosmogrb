4208ffa85fd4f4224a50cf6c45776119e3edacc0
grburgess <jmichaelburgess@gmail.com>
2020-04-03 09:14:48 +0200

    use function to get the usr dir path

--- cosmogrb/config.py
+++ cosmogrb/config.py
@@ -1,5 +1,7 @@
 from configya import YAMLConfig
 
+from cosmogrb.utils.package_utils import get_path_of_user_dir
+
 structure = {}
 structure["logging"] = dict(level="INFO")
 structure["multiprocess"] = dict(n_grb_workers=6, n_universe_workers=6)
@@ -10,7 +12,7 @@ class CosmogrbConfig(YAMLConfig):
 
         super(CosmogrbConfig, self).__init__(
             structure=structure,
-            config_path="~/.cosmogrb",
+            config_path=get_path_of_user_dir(),
             config_name="cosmogrb_config.yml",
         )
 

15477d9af0e2fca66b3a40647690eff3be6f25ce
grburgess <jmichaelburgess@gmail.com>
2020-04-04 15:43:34 +0200

    fix string saving type

--- cosmogrb/io/detector_save.py
+++ cosmogrb/io/detector_save.py
@@ -1,4 +1,7 @@
 import h5py
+import pandas as pd
+from IPython.display import display
+import collections
 from cosmogrb.utils.hdf5_utils import recursively_load_dict_contents_from_group
 
 
@@ -54,3 +57,36 @@ class DetectorSave(object):
                 extra_info = None
 
             return cls(name, is_detected, extra_info)
+
+    def info(self):
+
+        self._output(as_display=True)
+
+    def _output(self, as_display=False):
+
+        std_dict = collections.OrderedDict()
+
+        std_dict["name"] = self._name
+        std_dict["is_detected"] = self._is_detected
+
+        if as_display:
+
+            std_df = pd.Series(data=std_dict, index=std_dict.keys())
+
+            display(std_df.to_frame())
+
+            if self._extra_info:
+
+                extra_df = pd.Series(
+                    data=self._extra_info, index=self._extra_info.keys()
+                )
+
+                display(extra_df.to_frame())
+
+        else:
+
+            if self._extra_info:
+                for k, v in self._extra_info.items():
+                    std_dict[k] = v
+
+        return pd.Series(data=std_dict, index=std_dict.keys())

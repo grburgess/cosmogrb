4902751e91143f7fa9c669957b414ee451f19bda
grburgess <jmichaelburgess@gmail.com>
2020-04-04 12:21:58 +0200

    add process

--- cosmogrb/instruments/gbm/process_gbm_universe.py
+++ cosmogrb/instruments/gbm/process_gbm_universe.py
@@ -7,20 +7,27 @@ def process_gbm_universe(*grb_save_files, client=None, threshold=4.5, serial=Fal
 
         assert client is not None, "One must provide a client to process in parallel"
     
-    args = []
-    for grb_file in grb_save_files:
+        args = []
+        for grb_file in grb_save_files:
 
-        args.append([grb_file, threshold])
-    
-    futures = client.map(_submit, args)
-    client.gather(futures)
-    
+            args.append([grb_file, threshold])
+
+        futures = client.map(_submit, args)
+        client.gather(futures)
+
+    else:
+
+        for grb_file in grb_save_files:
+
+            _submit([grb_file, threshold])
+            
 
 def _submit(args):
 
     grb_file, threshold = args
     
     gbm_trigger = GBMTrigger(grb_save_file_name=grb_file, threshold=threshold)
+    gbm_trigger.process()
     gbm_trigger.save()
 
     

70f54f988c9bc6e54c53b096ec0d2c1857b3fb21
grburgess <jmichaelburgess@gmail.com>
2021-06-19 12:11:53 +0200

    added subphoto stuffs

---
+++ cosmogrb/sampler/subphoto.py
@@ -0,0 +1,227 @@
+
+from typing import Iterable
+
+import numba as nb
+import numpy as np
+
+from cosmogrb.sampler.subphoto_functions import (energy_integrated_evolution,
+                                                 sample_energy, sample_events,
+                                                 subphoto, subphoto_evolution,
+                                                 time_integrated_evolution)
+
+from .source_function import SourceFunction
+
+
+class SubPhoto(SourceFunction):
+    def __init__(
+            self, K=1,  xi_b = 1e-6, r_i=1e11, r_0=1e7, gamma=400, l_grb=1e54, emin=10.0, emax=1e4, response=None,
+    ):
+
+        """TODO describe function
+
+        :param K: 
+        :type K: 
+        :param xi_b: 
+        :type xi_b: 
+        :param r_i: 
+        :type r_i: 
+        :param r_0: 
+        :type r_0: 
+        :param gamma: 
+        :type gamma: 
+        :param l_grb: 
+        :type l_grb: 
+        :param emin: 
+        :type emin: 
+        :param emax: 
+        :type emax: 
+        :param response: 
+        :type response: 
+        :returns: 
+
+        """
+        # attach variables
+        self._K = K
+        self._xi_b = xi_b
+        self._r_i = r_i
+        self._r_0 = r_0
+        self._gamma = gamma
+        self._l_grb = l_grb
+
+
+        self._pre_comp_ei = None
+        
+        super(SubPhoto, self).__init__(
+            emin=emin, emax=emax, index=None, response=response)
+
+
+        self._differential_flux = lambda x: subphoto(x, xi_b, r_i, r_0, gamma, l_grb, self._z)
+
+    def set_integral_function(self):
+        
+        def integral(e1, e2):
+            # Trapz rule
+            if isinstance(e1, Iterable):
+                # Energy given as list or array
+
+                # Make sure we do not calculate the flux two times at the same energy
+                e_edges = np.append(e1, e2[-1])
+                diff_fluxes_edges = self._differential_flux(e_edges)
+                
+                return _trapz(np.array([diff_fluxes_edges[:-1], diff_fluxes_edges[1:]]).T, np.array([e1, e2]).T)
+            else:
+                # single energy values given
+                return _trapz(np.array([self._differential_flux(e1), self._differential_flux(e2)]), np.array([e1, e2]))
+
+        self._integral_function = integral
+                              
+
+        
+    def evolution(self, energy, time):
+
+        """TODO describe function
+
+        :param energy: 
+        :type energy: 
+        :param time: 
+        :type time: 
+        :returns: 
+
+        """
+        # call the numba function for speed
+        return subphoto_evolution(
+            energy=np.atleast_1d(energy),
+            time=np.atleast_1d(time),
+            K=self._K,
+            xi_b=self._xi_b,
+            r_i=self._r_i,
+            r_0=self._r_0,
+            gamma=self._gamma,
+            l_grb=self._l_grb,
+            z=self._z
+        )
+
+    def time_integrated_spectrum(self, energy, tmin, tmax):
+
+        """TODO describe function
+
+        :param energy: 
+        :type energy: 
+        :param tmin: 
+        :type tmin: 
+        :param tmax: 
+        :type tmax: 
+        :returns: 
+
+        """
+        return time_integrated_evolution(
+            energy=np.atleast_1d(energy),
+            tmin=tmin,
+            tmax=tmax,
+            K=self._K,
+            xi_b=self._xi_b,
+            r_i=self._r_i,
+            r_0=self._r_0,
+            gamma=self._gamma,
+            l_grb=self._l_grb,
+            effective_area=self._response.effective_area_packed,
+            z=self._z
+        )
+
+    def energy_integrated_evolution(self, time):
+
+        ea = self._response.effective_area_packed
+
+
+        if self._pre_comp_ei is None:
+            self._pre_comp_ei = energy_integrated_evolution(
+                time=np.atleast_1d(time),
+                K=self._K,
+                xi_b=self._xi_b,
+                r_i=self._r_i,
+                r_0=self._r_0,
+                gamma=self._gamma,
+                l_grb=self._l_grb,
+                emin=self._emin,
+                emax=self._emax,
+                effective_area=ea,
+                z=self._z,
+                precompute=self._pre_comp_ei
+            )
+
+        return self._pre_comp_ei
+
+    def sample_events(self, tstart, tstop, fmax):
+
+        """TODO describe function
+
+        :param tstart: 
+        :type tstart: 
+        :param tstop: 
+        :type tstop: 
+        :param fmax: 
+        :type fmax: 
+        :returns: 
+
+        """
+
+        
+        return sample_events(
+            tstart=tstart,
+            tstop=tstop,
+            K=self._K,
+            xi_b=self._xi_b,
+            r_i=self._r_i,
+            r_0=self._r_0,
+            gamma=self._gamma,
+            l_grb=self._l_grb,
+            emin=self._emin,
+            emax=self._emax,
+            effective_area=self._response.effective_area_packed,
+            fmax=fmax,
+            z=self._z
+        )
+
+    def sample_energy(self, times):
+        """TODO describe function
+
+        :param times: 
+        :type times: 
+        :returns: 
+
+        """
+        
+        ea = self._response.effective_area_packed
+
+        _ = sample_energy(
+            times=times[:2],
+            K=self._K,
+            xi_b=self._xi_b,
+            r_i=self._r_i,
+            r_0=self._r_0,
+            gamma=self._gamma,
+            l_grb=self._l_grb,
+            emin=self._emin,
+            emax=self._emax,
+            effective_area=ea,
+            z=self._z)
+
+        return sample_energy(
+            times=times,
+            K=self._K,
+            xi_b=self._xi_b,
+            r_i=self._r_i,
+            r_0=self._r_0,
+            gamma=self._gamma,
+            l_grb=self._l_grb,
+            emin=self._emin,
+            emax=self._emax,
+            effective_area=ea,
+            z=self._z
+            
+        )
+
+
+@nb.njit(fastmath=True, cache=True)
+def _trapz(x, y):
+    return np.trapz(x, y)
---
+++ cosmogrb/sampler/subphoto_functions.py
@@ -0,0 +1,294 @@
+import collections
+import math
+import pickle
+from functools import update_wrapper, wraps
+from pathlib import Path
+
+import h5py
+import numba as nb
+import numba_scipy
+import numpy as np
+from interpolation import interp
+from interpolation.splines import eval_linear
+from scipy.special import gamma, gammaincc
+
+from cosmogrb.sampler.cpl_functions import cpl
+from cosmogrb.utils.numba_array import VectorFloat64
+from cosmogrb.utils.package_utils import get_path_of_data_file
+
+file_name = get_path_of_data_file("subphoto.h5")
+
+with h5py.File(file_name, "r") as f:
+
+    # name = f.attrs["name"]
+    # description = f.attrs["description"] 
+    # interpolation_degree = f.attrs["interpolation_degree"] 
+    # spline_smoothing_factor = f.attrs["spline_smoothing_factor"] 
+    
+    energies = f["energies"][()]
+    parameter_order = f["parameter_order"][()]
+    
+    grid = np.ascontiguousarray(f["grid"][()])
+    
+    parameters = collections.OrderedDict()
+    
+    for k in parameter_order:
+        
+        parameters[k] = f["parameters"][k][()]
+
+
+data_shape = tuple([x.shape[0] for x in list(parameters.values())])
+
+_values = tuple([np.array(x) for x in list(parameters.values())])
+
+n_energies = len(energies)
+
+@nb.njit(fastmath=True)
+def subphoto(energy, xi_b, r_i, r_0, gamma, l_grb, z):
+    
+    
+    scale = gamma/ (1.+ z)
+    
+    param_values = np.array([xi_b, r_i, r_0, gamma, l_grb])
+    
+    log_energies = np.log10(energy)
+    
+    log_interpolations = np.empty(n_energies)
+    
+    
+    for i in range(n_energies):
+
+#         this_data = np.array(
+#                         ,
+#                         #dtype=float,
+#                     )
+
+        log_interpolations[i] = eval_linear( _values,
+                                            np.log10(grid[...,i]).reshape(*data_shape), 
+                                            param_values)
+
+    e_tilde = np.log10(energies * scale)
+    
+    values = np.power(10., interp(e_tilde, log_interpolations, log_energies))
+    
+    return values / scale
+
+
+
+@nb.njit(fastmath=True, cache=False)
+def subphoto_evolution(energy, time, K, xi_b, r_i, r_0, gamma, l_grb,  z):
+    """
+    evolution of the SUBPHOTO function with time
+
+    :param energy:
+    :param time:
+    :param peak_flux:
+    :param ep_start:
+    :param ep_tau:
+    :param alpha:
+    :param trise:
+    :param tdecay:
+    :param emin:
+    :param emax:
+    :returns:
+    :rtype:
+
+    """
+
+    N = time.shape[0]
+    M = energy.shape[0]
+
+    out = np.empty((N, M))
+
+    arg = K * subphoto( energy,  xi_b, r_i, r_0, gamma, l_grb,z)
+
+    for n in range(N):
+            out[n, :] = arg
+            
+
+    return out
+
+
+@nb.njit(fastmath=True, cache=False)
+def folded_subphoto_evolution(
+    energy,
+    time,
+    K,
+    xi_b,
+    r_i,
+    r_0,
+    gamma,
+    l_grb,
+    response,
+    z,
+):
+
+    return interp(response[0], response[1], energy) * subphoto_evolution(
+        energy, time, K, xi_b, r_i, r_0, gamma, l_grb, z
+    )
+
+
+@nb.njit(fastmath=True, cache=False)
+def sample_events(
+    emin,
+    emax,
+    tstart,
+    tstop,
+    K,
+    xi_b,
+    r_i,
+    r_0,
+    gamma,
+    l_grb,
+    effective_area,
+    fmax,
+    z,
+):
+
+    time = tstart
+
+    arrival_times = VectorFloat64(0)
+    arrival_times.append(time)
+
+    vtime = np.empty(1)
+    while True:
+
+        time = time - (1.0 / fmax) * np.log(np.random.rand())
+        if time > tstop:
+            break
+
+        test = np.random.rand()
+
+        vtime[0] = time
+
+        # p_test = (
+        #     energy_integrated_evolution(
+        #         emin, emax, vtime, K, xi_b, r_i, r_0, gamma, l_grb, effective_area, z
+        #     )
+        #     / fmax
+        # )
+
+        p_test = 1.
+        
+        if test <= p_test:
+            arrival_times.append(time)
+
+    return arrival_times.arr
+
+
+@nb.njit(fastmath=True, cache=False, parallel=True)
+def sample_energy(times, K, xi_b, r_i, r_0, gamma, l_grb, emin, emax, effective_area, z):
+
+    N = times.shape[0]
+
+    egrid = np.power(10., np.linspace(np.log10(emin), np.log10(emax), 50))
+
+    out = np.zeros(N)
+
+    times = np.array([times[1]])
+
+    
+    tmps = folded_subphoto_evolution(
+        egrid, times, K, xi_b, r_i, r_0, gamma, l_grb, effective_area, z
+    )
+
+    tmp = tmps[0, :]
+
+    idx = np.argmax(tmp * egrid**2)
+
+    # bump up C just in case
+
+    C = tmp[idx] * 3
+    alpha = -1.1
+
+
+    x = np.empty(1)
+    vtime = np.empty(1)
+    for i in nb.prange(N):
+        
+        
+
+        # the maximum is either at the lower bound or the max effective area
+
+     
+        # so this scheme for dealing with the effective area
+        # is likely very fragile. The idea is that power law
+        # envelope needs to be greater than the function every where
+
+        # if alpha == -1.0:
+        #     alpha = -1 + 1e-20
+
+        while True:
+
+            # sample from a power law
+            u = np.random.uniform(0, 1)
+            x[0] = np.power(
+                (math.pow(emax, alpha + 1) - math.pow(emin, alpha + 1)) * u
+                + math.pow(emin, alpha + 1),
+                1.0 / (alpha + 1.0),
+
+            )
+
+#            x[0] = np.random.uniform(emin, emax)
+            
+            y = np.random.uniform(0, 1) * C * math.pow(x[0] / egrid[idx], alpha)
+
+            # here the vtime is just to trick this into being an array
+
+            
+            
+            vtime[0] = times[0]
+
+            if (
+                y
+                <= (
+                    folded_subphoto_evolution(
+                        x, vtime, K, xi_b, r_i, r_0, gamma, l_grb, effective_area, z
+                    )
+                )[0, 0]
+            ):
+
+                out[i] = x[0]
+                break
+
+    return out
+
+
+@nb.njit(fastmath=True, cache=False)
+def energy_integrated_evolution(
+        emin, emax, time, K, xi_b, r_i, r_0, gamma, l_grb, effective_area, z, precompute=None
+):
+
+
+    if precompute is not None:
+
+        return precompute
+    
+    n_energies = 75
+
+    energy_grid = np.power(10., np.linspace(np.log10(emin), np.log10(emax), n_energies))
+
+
+    energy_slice = folded_subphoto_evolution(
+        energy_grid, time, K,  xi_b, r_i, r_0, gamma, l_grb, effective_area, z
+    )
+
+    return np.trapz(energy_slice[0, :], energy_grid)
+
+
+@nb.njit(fastmath=True, cache=False)
+def time_integrated_evolution(
+    tmin, tmax, energy,  xi_b, r_i, r_0, gamma, l_grb, effective_area, z
+):
+
+
+    n_times = 5
+
+    time_grid = np.linspace(tmin, tmax, n_times)
+
+    time_slice = folded_subphoto_evolution(
+        energy, time_grid,  xi_b, r_i, r_0, gamma, l_grb,  effective_area, z
+    )
+
+    
+    return np.trapz(time_slice[:, 0], time_grid)
